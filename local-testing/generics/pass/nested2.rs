e11ff6d0cd69c6b2b9170d5eb03bb95c59fb9b5a
Zack Grannan <zgrannan@protonmail.com>
2025-04-30 13:41:42 -0700

    move tests

--- local-testing/generics/pass/nested2.rs
+++
@@ -1,7 +0,0 @@
-struct R(Option<Box<u32>>);
-fn main() {
-    match R(None).0 {
-        Some(_) => (),
-        _ => (),
-    }
-}
--- local-testing/rpe.rs
+++
@@ -1,8 +0,0 @@
-
-fn f(x: &mut i32, y: &mut i32, z: &mut i32) {
-    let old_y = *y;
-    *x = 1;
-    assert!(*y == old_y);
-}
-
-fn main(){}

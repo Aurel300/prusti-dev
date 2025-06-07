f7638d323c590b3b56f24fb5a7711370cac04c16
paaassscccaaalll <pphollenstein@gmail.com>
2025-06-07 10:06:21 +0200

    new axiom for immref
    added my functionality to up to date branch
    
    deleted pcs
    
    deleted utils and put helper function into
    
    change is_ad_trusted() to is_type_trusted() which now take in a ty:TY
    
    not using a filter anymore
    
    formating fix PR 68
    
    Update prusti-encoder/src/encoders/mod.rs
    
    removed comment
    
    Co-authored-by: Aurel <Aurel300@users.noreply.github.com>
    
    removed Trusted from Domain
    
    moved trusted handeling to predicate.rs
    
    changed prusti_rustc_interface::middle::ty::Ty to use ty::Ty directly
    
    style improvement

--- prusti-encoder/src/encoders/type/kinds/immref.rs
+++ prusti-encoder/src/encoders/type/kinds/immref.rs
@@ -1,16 +1,18 @@
 use crate::encoders::{
-    domain::{DomainBuilder, DomainDataImmRef, DomainEnc, DomainEncSpecifics},
+    domain::{DomainBuilder, DomainDataImmRef, DomainEnc, DomainEncSpecifics, DomainEncOutputRef},
     predicate::{PredicateBuilder, PredicateEncData, PredicateEncDataImmRef},
     rust_ty_snapshots::RustTySnapshotsEnc,
     snapshot::SnapshotEncOutput,
     GenericEnc, PredicateEnc,
 };
+use crate::TyConstructorEnc;
 use prusti_rustc_interface::middle::ty;
 use task_encoder::{EncodeFullError, TaskEncoder, TaskEncoderDependencies};
 use vir::ToKnownArity;
 
 pub(crate) fn domain<'vir>(
     task_key: <DomainEnc as TaskEncoder>::TaskKey<'vir>,
+    output_ref: &DomainEncOutputRef<'vir>,
     deps: &mut TaskEncoderDependencies<'vir, DomainEnc>,
     builder: &mut DomainBuilder<'vir>,
 ) -> Result<DomainEncSpecifics<'vir>, EncodeFullError<'vir, DomainEnc>> {
@@ -29,11 +31,41 @@ pub(crate) fn domain<'vir>(
     let value_ident = builder.function("value", &[builder.self_type()], inner_type);
     let cons_ident = builder.function("cons", &[prim_type, inner_type], builder.self_type());
 
+<<<<<<< HEAD
+    let generic_enc = deps.require_ref::<GenericEnc>(())?;
+    let ty_type_func = deps.require_ref::<TyConstructorEnc>(task_key)?;
+=======
+    let ref_immutable_cons_func = vir::FunctionIdent::new(
+        vir::ViperIdent::new("s_Ref_immutable_cons"),
+        vir::UnknownArity::new(builder.vcx.alloc_slice(&[prim_type, param_type])),
+        ref_immutable_type,
+    );
+    let ref_immutable_typeof_func = vir::FunctionIdent::new(
+        vir::ViperIdent::new("s_Ref_immutable_typeof"),
+        vir::UnknownArity::new(builder.vcx.alloc_slice(&[ref_immutable_type])),
+        type_type,
+    );
+    let typ_func = vir::FunctionIdent::new(
+        vir::ViperIdent::new("typ"),
+        vir::UnknownArity::new(builder.vcx.alloc_slice(&[param_type])),
+        type_type,
+    );
+    let ref_immutable_type_func = vir::FunctionIdent::new(
+        vir::ViperIdent::new("s_Ref_immutable_type"),
+        vir::UnknownArity::new(builder.vcx.alloc_slice(&[type_type])),
+        type_type,
+    );
+>>>>>>> 602b5076dfe (style improvement)
     builder.axiom("deref", vir::expr! {
         forall r: [prim_type], value: [inner_type] :: {[cons_ident](r, value)} ([deref_ident]([cons_ident](r, value))) == (r)
     });
     builder.axiom("value", vir::expr! {
         forall r: [prim_type], value: [inner_type] :: {[cons_ident](r, value)} ([value_ident]([cons_ident](r, value))) == (value)
+    });
+    builder.axiom("ref_immutable_cons_typeof", vir::expr! {
+        forall r: [prim_type], p: [inner_type] ::
+            {[output_ref.typeof_function]([cons_ident](r, p))}
+            ([output_ref.typeof_function]([cons_ident](r, p))) == ([ty_type_func.ty_constructor]([generic_enc.param_type_function](p)))
     });
     // builder.axiom("cons", vir::expr! {
     //     forall s: [builder.self_type()] :: {[deref_ident](s)} ([cons_ident]([deref_ident](s))) == (s)

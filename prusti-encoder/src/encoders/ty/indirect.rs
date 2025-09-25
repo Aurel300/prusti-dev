d4f87ccc3c57eb3e0a99afba55d490a9ce7a5743
Zack Grannan <zgrannan@protonmail.com>
2025-09-25 07:09:06 -0700

    WIP

--- prusti-encoder/src/encoders/ty/indirect.rs
+++ prusti-encoder/src/encoders/ty/indirect.rs
@@ -3,7 +3,7 @@ use prusti_rustc_interface::middle::ty::{self};
 use task_encoder::{EncodeFullResult, TaskEncoder, TaskEncoderDependencies};
 use vir::{CastType, Reify};
 
-use crate::encoders::ty::{pure::TyPureEnc, RustTyDecomposition};
+use crate::encoders::ty::{RustTyDecomposition, pure::TyPureEnc};
 
 use super::{data::TySpecifics, use_impure::TyUseImpureEnc, use_pure::TyUsePureEnc};
 
@@ -82,15 +82,17 @@ impl TaskEncoder for IndirectPredicatesEnc {
         deps: &mut TaskEncoderDependencies<'vir, Self>,
     ) -> EncodeFullResult<'vir, Self> {
         deps.emit_output_ref(*task_key, ())?;
+        eprintln!("Encoding indirect predicates for {:?}", task_key);
         vir::with_vcx(|vcx| {
             let ty = task_key.base();
             let self_ty_enc = deps.require_dep::<TyUsePureEnc>(ty)?;
             let combined = ty.ty.zip(self_ty_enc);
-            let predicate_applications = match combined.specifics {
+            let mut predicate_applications = vec![];
+            match combined.specifics {
                 TySpecifics::MutRef((data, ref_domain)) => {
                     let inner_ty = data.decompose_normalize(ty.args);
                     let inner_ty_enc = deps.require_dep::<TyUseImpureEnc>(inner_ty)?;
-                    vec![vcx.mk_lazy_expr(
+                    predicate_applications.push(vcx.mk_lazy_expr(
                         "ref_indirect",
                         vir::TYPE_BOOL,
                         Box::new(move |vcx, self_expr: vir::ExprSnap<'vir>| {
@@ -102,12 +104,67 @@ impl TaskEncoder for IndirectPredicatesEnc {
                                 )
                                 .kind
                         }),
-                    )]
+                    ));
+                    let new_projection =
+                        LifetimeProjection::new(task_key.region(()), inner_ty, None, ()).unwrap();
+                    let inner_indirect =
+                        deps.require_dep::<IndirectPredicatesEnc>(new_projection)?;
+                    predicate_applications.extend(
+                        inner_indirect
+                            .predicate_applications
+                            .into_iter()
+                            .map(|inner_expr| {
+                                vcx.mk_lazy_expr(
+                                    "ref_inner_indirect",
+                                    vir::TYPE_BOOL,
+                                    Box::new(move |vcx, self_expr: vir::ExprGenSnap<_, _>| {
+                                        inner_expr
+                                            .reify(
+                                                vcx,
+                                                ref_domain.value_access(self_expr.downcast_ty()),
+                                            )
+                                            .kind
+                                    }),
+                                )
+                            }),
+                    );
+                }
+                TySpecifics::StructLike(data) => {
+                    for (field_ty, accessor) in data.fields {
+                        let project = |inner_expr: ExprOutput<'vir>| {
+                            vcx.mk_lazy_expr(
+                                "ref_inner_indirect",
+                                vir::TYPE_BOOL,
+                                Box::new(move |vcx, self_expr: vir::ExprGenSnap<_, _>| {
+                                    inner_expr
+                                        .reify(vcx, accessor.read(self_expr.downcast_ty()))
+                                        .kind
+                                }),
+                            )
+                        };
+
+                        // TODO: invalid recursion here if the defined struct is
+                        // recursive!
+                        let field_ty = field_ty.decompose(ty.ty.params);
+                        let new_projection =
+                            LifetimeProjection::new(task_key.region(()), field_ty, None, ()).unwrap();
+                        let field_indirect =
+                            deps.require_dep::<IndirectPredicatesEnc>(new_projection)?;
+                        predicate_applications.extend(
+                            field_indirect
+                                .predicate_applications
+                                .into_iter()
+                                .map(project),
+                        );
+                    }
                 }
                 // TODO: recurse into other types
-                _ => vec![],
+                _ => {}
             };
-            Ok(((), IndirectPredicatesEncOutputRef::new(predicate_applications)))
+            Ok((
+                (),
+                IndirectPredicatesEncOutputRef::new(predicate_applications),
+            ))
         })
     }
 }

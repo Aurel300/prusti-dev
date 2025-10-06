f17e1610c056597584935f091ee25b4170a5f1c9
Zack Grannan <zgrannan@protonmail.com>
2025-10-06 16:53:15 -0700

    remove unused

--- prusti-tests/tests/v2/pass/generics/nested2.rs
+++ prusti-tests/tests/v2/pass/generics/nested2.rs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:prusti-tests/tests/v2/pass/generics/nested2.rs
 /*struct R(Option<Box<u32>>);
 fn main() {
     match R(None).0 {
@@ -7,9 +6,6 @@ fn main() {
     }
 }*/
 struct R(Option<u32>);
-=======
-struct R(Option<Box<u32>>);
->>>>>>> ide/rewrite-2023-assistant-features:local-testing/generics/nested2.rs
 fn main() {
     match R(None).0 {
         Some(_) => (),

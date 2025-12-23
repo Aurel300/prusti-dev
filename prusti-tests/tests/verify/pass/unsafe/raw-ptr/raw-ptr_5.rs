2f41dc12ef6f9e8bad012a8e4159c23ca600af9b
Thomas Mayerl <tmayerl@student.ethz.ch>
2025-12-23 15:05:23 +0100

    Add test for future support of fractional permissions

---
+++ prusti-tests/tests/verify/pass/unsafe/raw-ptr/raw-ptr_5.rs
@@ -0,0 +1,12 @@
+use prusti_contracts::*;
+
+fn foo() {
+    let mut x = 5;
+    let r_x = &raw mut x;
+    bar(r_x);
+    assert!(x == 5);
+}
+
+#[requires(#[prusti::frac(1/2)] acc(x))]
+#[ensures(#[prusti::frac(1/2)] acc(x))]
+fn bar(x: *mut i32) {}

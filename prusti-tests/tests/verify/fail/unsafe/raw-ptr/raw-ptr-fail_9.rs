4cc65dfe7fac5ca17ba83fc36137d7202d21213b
Thomas Mayerl <tmayerl@student.ethz.ch>
2026-01-20 09:34:17 +0100

    Improve tests

--- prusti-tests/tests/verify/fail/unsafe/raw-ptr/raw-ptr-fail_9.rs
+++ prusti-tests/tests/verify/fail/unsafe/raw-ptr/raw-ptr-fail_9.rs
@@ -4,12 +4,11 @@ fn foo() {
     let mut x = 5;
     let r_x = &mut x;
     let p_x = r_x as *mut i32;
-    bar(r_x);
+    // Upon writing to r_x, p_x should become invalid because the aliasing rules of r_x
+    // require r_x to be unique
+    *r_x = 6;
     unsafe {
-        assert!(*p_x == 5);
+        // UB: p_x should be invalid
+        assert!(*p_x == 6); 
     }
-}
-
-fn bar(x: &mut i32) {
-    *x = 6;
 }

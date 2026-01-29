1ff9553d169a05439df2782e9786900b1a9dad12
Jonáš Fiala <jonas.fiala@inf.ethz.ch>
2026-01-29 13:32:07 +0100

    Resolve compilation warnings

--- prusti-contracts/prusti-contracts/src/core_spec/mod.rs
+++ prusti-contracts/prusti-contracts/src/core_spec/mod.rs
@@ -8,6 +8,7 @@ pub use eq::PureEq;
 pub(super) mod type_eq {
     /// A trait which can be used as a bound to say that two types are the same. For
     /// example `Self: TypeEq<Rhs>` can be used as a condition in `PartialEq`.
+    #[allow(private_bounds)]
     pub trait TypeEq<T>: SealedTypeEq<T> {}
     impl<T> TypeEq<T> for T {}
 

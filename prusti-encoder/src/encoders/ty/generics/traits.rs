4a99e61d0a691bb1bb777b03085fe48b57327a68
Trzyq0712 <j.trzyq@gmail.com>
2026-02-15 22:46:17 +0100

    Trait impl check for unknown types

--- prusti-encoder/src/encoders/ty/generics/traits.rs
+++ prusti-encoder/src/encoders/ty/generics/traits.rs
@@ -1,7 +1,7 @@
 use prusti_rustc_interface::{middle::ty::AssocKind, span::def_id::DefId};
 use rustc_hash::FxHashMap;
 use task_encoder::{EncodeFullResult, TaskEncoder, TaskEncoderDependencies};
-use vir::{CallableIdn, FunctionIdn, vir_format_identifier};
+use vir::{AdtDestructorWrapper, CallableIdn, FunctionIdn, vir_format_identifier};
 
 use crate::encoders::ty::{
     RustTyDecomposition,
@@ -22,6 +22,7 @@ pub struct TraitData<'vir> {
 pub struct TraitEncOutput<'vir> {
     trait_domain: vir::Domain<'vir>,
     impl_fun: vir::Function<'vir>,
+    impl_fun_unknown: vir::Function<'vir>,
 }
 
 impl TaskEncoder for TraitEnc {
@@ -40,6 +41,7 @@ impl TaskEncoder for TraitEnc {
         for trait_enc in TraitEnc::all_outputs_local_no_errors() {
             program.add_domain(trait_enc.trait_domain);
             program.add_function(trait_enc.impl_fun);
+            program.add_function(trait_enc.impl_fun_unknown);
         }
     }
 
@@ -65,8 +67,7 @@ impl TaskEncoder for TraitEnc {
                         FunctionIdn::new(
                             vir_format_identifier!(
                                 vcx,
-                                "{}_Assoc_{}_func",
-                                trait_name,
+                                "{trait_name}_Assoc_{}_func",
                                 tcx.item_name(item.def_id),
                             ),
                             vcx.alloc_slice(&vec![vir::TYPE_TYVAL; params_type.ty_exprs().len()]), // params_type also includes parameters of trait itself
@@ -81,13 +82,30 @@ impl TaskEncoder for TraitEnc {
                 .collect::<Vec<_>>();
 
             let impl_fun_idn = FunctionIdn::new(
-                vir_format_identifier!(vcx, "{}_impl", trait_name),
+                vir_format_identifier!(vcx, "{trait_name}_impl"),
                 vcx.alloc_slice(&(vec![vir::TYPE_TYVAL; params.ty_exprs().len()])),
                 vir::TYPE_BOOL,
             );
 
+            let impl_fun_unknown_idn: FunctionIdn<'vir, vir::Int, vir::Bool> = FunctionIdn::new(
+                vir_format_identifier!(vcx, "{trait_name}_impl_unknown"),
+                vir::TYPE_INT,
+                vir::TYPE_BOOL,
+            );
+
+            let impl_fun_unknown = vcx.mk_function(
+                impl_fun_unknown_idn,
+                (vcx.mk_local_decl("non_unit", vir::TYPE_INT),),
+                &[],
+                &[],
+                None,
+                None,
+            );
+
             let impl_fun_body = {
-                let mut checks = Vec::new();
+                let impl_type_expr = params.ty_exprs()[0];
+
+                let mut trait_impl_checks = Vec::new();
                 for impl_did in tcx.all_impls(*task_key) {
                     let implementing_ty = tcx.type_of(impl_did).instantiate_identity();
                     let implementing_ty = RustTyDecomposition::from_ty(implementing_ty, impl_did);
@@ -95,13 +113,23 @@ impl TaskEncoder for TraitEnc {
                     let impl_type = deps.require_ref::<TyConstructorEnc>(implementing_ty.ty)?;
 
                     let type_check = vcx.mk_adt_discriminator_expr(
-                        params.ty_exprs()[0],
+                        impl_type_expr,
                         impl_type.ty_constructor.name().to_str(),
                     );
-                    checks.push(type_check);
+                    trait_impl_checks.push(type_check);
                 }
 
-                vcx.mk_disj(&checks)
+                // Check for types outside of the known type enumeration
+                let unknown_type_check = {
+                    let type_check = vcx.mk_adt_discriminator_expr(impl_type_expr, "Unknown_type");
+                    let unknown_type_destructor =
+                        vcx.mk_adt_destructor("non_unit", vir::TYPE_TYVAL, vir::TYPE_INT);
+                    let unknown_type_id = unknown_type_destructor.call()(impl_type_expr);
+                    vcx.mk_conj(&[type_check, impl_fun_unknown_idn(unknown_type_id)])
+                };
+                trait_impl_checks.push(unknown_type_check);
+
+                vcx.mk_disj(&trait_impl_checks)
             };
 
             let impl_fun = vcx.mk_function(
@@ -114,7 +142,7 @@ impl TaskEncoder for TraitEnc {
             );
 
             let trait_domain = vcx.mk_domain(
-                vir_format_identifier!(vcx, "t_{}", trait_name),
+                vir_format_identifier!(vcx, "t_{trait_name}"),
                 &[],
                 &[],
                 vcx.alloc_slice(funcs.as_slice()),
@@ -124,6 +152,7 @@ impl TaskEncoder for TraitEnc {
                 TraitEncOutput {
                     trait_domain,
                     impl_fun,
+                    impl_fun_unknown,
                 },
                 TraitData {
                     trait_name,

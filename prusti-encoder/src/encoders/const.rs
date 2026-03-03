6a88c58a8f3da4a56323baad3e1e13d33f3f0ebd
Thomas Mayerl <tmayerl@student.ethz.ch>
2026-03-03 17:51:42 +0100

    Fix tests by reimplementing control-flow for ty consts

--- prusti-encoder/src/encoders/const.rs
+++ prusti-encoder/src/encoders/const.rs
@@ -5,7 +5,10 @@ use prusti_rustc_interface::{
         interpret::{CtfeProvenance, InterpCx, Projectable},
     },
     middle::{
-        mir::{self, ConstValue},
+        mir::{
+            self, ConstValue,
+            interpret::{GlobalAlloc, Scalar},
+        },
         ty,
         ty::{TyKind, TypingEnv},
     },
@@ -62,10 +65,59 @@ impl ConstEnc {
             }
             ty::ConstKind::Value(val) => {
                 let val = vir::with_vcx(|vcx| vcx.tcx().valtree_to_const_val(val));
-                Self::encode_const_val(deps, val, ty, context, None)
+                Self::encode_const_val_ty(deps, val, ty, context)
             }
             k => todo!("const kind {k:?}"),
         }
+    }
+
+    fn encode_const_val_ty<'vir>(
+        deps: &mut TaskEncoderDependencies<'vir, Self>,
+        val: ConstValue,
+        ty: ty::Ty<'vir>,
+        context: GParams<'vir>,
+    ) -> Result<vir::ExprCSnap<'vir>, EncodeFullError<'vir, Self>> {
+        vir::with_vcx(|vcx| {
+            let ty_task = RustTyDecomposition::from_ty(ty, context);
+            let kind = deps.require_dep::<TyUsePureEnc>(ty_task)?;
+            Ok(match val {
+                ConstValue::Scalar(Scalar::Int(int)) => {
+                    let prim = kind.expect_primitive();
+                    let val = int.to_bits(int.size());
+                    let val = prim.expr_from_bits(ty, val);
+                    (prim.prim_to_snap)(val)
+                }
+                ConstValue::Scalar(Scalar::Ptr(ptr, _)) => {
+                    match vcx.tcx().global_alloc(ptr.provenance.alloc_id()) {
+                        GlobalAlloc::Function { .. } => todo!(),
+                        GlobalAlloc::VTable(_, _) => todo!(),
+                        GlobalAlloc::Static(_) => todo!(),
+                        GlobalAlloc::Memory(_mem) => unreachable!(),
+                        GlobalAlloc::TypeId { .. } => todo!(),
+                    }
+                }
+                ConstValue::ZeroSized => {
+                    let s = kind.expect_structlike();
+                    s.field_snaps_to_snap(Vec::new())
+                }
+                // Encode `&str` constants to an opaque domain. If we ever want to perform string reasoning
+                // we will need to revisit this encoding, but for the moment this allows assertions to avoid
+                // crashing Prusti.
+                ConstValue::Slice { .. } if ty.peel_refs().is_str() => {
+                    let ref_ty = kind.expect_immref();
+                    let str_ty = ty.peel_refs();
+                    let str_ty_task = RustTyDecomposition::from_ty(str_ty, context);
+                    let str_snap = deps.require_dep::<TyUsePureEnc>(str_ty_task)?;
+                    let str_snap = str_snap.expect_opaque();
+                    // first, we create a string snapshot
+                    let snap = (str_snap.arbitrary)().upcast_ty();
+                    // wrap it in a ref
+                    vir::with_vcx(|vcx| ref_ty.prim_to_snap(vcx.mk_null(), snap))
+                }
+                ConstValue::Slice { .. } => todo!("ConstValue::Slice: {ty:?}"),
+                ConstValue::Indirect { .. } => todo!("ConstValue::Indirect"),
+            })
+        })
     }
 
     fn encode_const_val_tree<'vir, T>(
@@ -247,9 +299,9 @@ impl ConstEnc {
         val: ConstValue,
         ty: ty::Ty<'vir>,
         context: GParams<'vir>,
-        span: Option<Span>,
+        span: Span,
     ) -> Result<vir::ExprCSnap<'vir>, EncodeFullError<'vir, Self>> {
-        let ty_ctxt_at = vir::with_vcx(|vcx| vcx.tcx().at(span.unwrap()));
+        let ty_ctxt_at = vir::with_vcx(|vcx| vcx.tcx().at(span));
         let (ecx, v) =
             mk_eval_cx_for_const_val(ty_ctxt_at, TypingEnv::fully_monomorphized(), val, ty)
                 .unwrap();
@@ -288,7 +340,7 @@ impl TaskEncoder for ConstEnc {
                 span,
             } => match const_ {
                 mir::Const::Val(val, ty) => {
-                    Self::encode_const_val(deps, val, ty, def_id.into(), Some(span))?
+                    Self::encode_const_val(deps, val, ty, def_id.into(), span)?
                 }
                 mir::Const::Unevaluated(uneval, ty) => vir::with_vcx(|vcx| {
                     let resolved = {
@@ -297,7 +349,7 @@ impl TaskEncoder for ConstEnc {
                             .const_eval_resolve(typing_env, uneval, vcx.tcx().def_span(def_id))
                     };
                     if let Ok(val) = resolved {
-                        Self::encode_const_val(deps, val, ty, def_id.into(), Some(span))
+                        Self::encode_const_val(deps, val, ty, def_id.into(), span)
                     } else if let Some(promoted) = uneval.promoted {
                         let task = MirPureEncTask {
                             encoding_depth: encoding_depth + 1,

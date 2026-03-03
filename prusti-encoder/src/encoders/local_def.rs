60ee0c09158fb23413e986cfe202f785d8ee1a89
Aurea Bílá <aurel.bily@gmail.com>
2026-03-03 13:52:21 +0100

    use context DefId for the typing env in local defs enc

--- prusti-encoder/src/encoders/local_def.rs
+++ prusti-encoder/src/encoders/local_def.rs
@@ -248,7 +248,7 @@ impl TaskEncoder for MirLocalDefEnc {
                     arg_count: body.arg_count,
                 }
             } else {
-                let typing_env = ty::TypingEnv::post_analysis(vcx.tcx(), task_key.def_id());
+                let typing_env = ty::TypingEnv::post_analysis(vcx.tcx(), task_key.context_def_id());
                 let sig = vcx.tcx().instantiate_and_normalize_erasing_regions(
                     task_key.substs(vcx),
                     typing_env,

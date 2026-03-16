fc742d0fca4d60ac2c24469aa3db1fec12b9db28
Richard Wohlbold <richard@rgwohlbold.de>
2026-03-16 20:52:39 +0100

    Refactor assert to clear up logic

--- prusti-encoder/src/encoders/ty/lifted/ty_constructor.rs
+++ prusti-encoder/src/encoders/ty/lifted/ty_constructor.rs
@@ -72,10 +72,10 @@ impl TaskEncoder for TyConstructorEnc {
         task_key: &Self::TaskKey<'vir>,
         deps: &mut task_encoder::TaskEncoderDependencies<'vir, Self>,
     ) -> EncodeFullResult<'vir, Self> {
-        assert!(
-            !task_key.specifics.is_param()
-                || matches!(&task_key.specifics, TySpecifics::Param(RustParamData::Dyn))
-        );
+        assert!(!matches!(
+            &task_key.specifics,
+            TySpecifics::Param(RustParamData::Generic)
+        ));
         vir::with_vcx(|vcx| {
             let base_name = task_key.name();
             let params = deps.require_dep::<GenericParamsEnc>(task_key.params)?;

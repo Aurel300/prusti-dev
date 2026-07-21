997ddfb46cd88917fa100ee10fe88dedea4fb176
Thomas Mayerl <tmayerl@student.ethz.ch>
2026-07-21 11:53:18 +0200

    move tests to fail

---
+++ prusti-tests/tests/verify/fail/casts/int_to_float.rs
@@ -0,0 +1,24 @@
+// The result of these casts can currently not be verified using Z3.
+// Once Z3 supports these cases, replace pass/casts/int_to_float.rs with this file.
+
+fn main() {
+    let x = 100;
+    let y = x as f32;
+    assert!(y == 100.0);
+
+    let x = u128::MAX;
+    let y = x as f32;
+    assert!(y == f32::INFINITY);
+
+    let x = u8::MAX;
+    let y = x as f64;
+    assert!(y == 255.0);
+
+    let x = -5;
+    let y = x as f32;
+    assert!(y == -5.0);
+
+    let x = 9007199254740993i64;
+    assert!(x == 9007199254740993);
+    assert!(x as f32 == 9007199254740992.0);
+}
--- prusti-tests/tests/verify/pass/casts/int_to_float.rs
+++ prusti-tests/tests/verify/pass/casts/int_to_float.rs
@@ -1,23 +1,15 @@
-// This currently works with CVC5 but not with Z3..
-
 fn main() {
     let x = 100;
     let y = x as f32;
-    assert!(y == 100.0);
 
     let x = u128::MAX;
     let y = x as f32;
-    assert!(y == f32::INFINITY);
 
     let x = u8::MAX;
     let y = x as f64;
-    assert!(y == 255.0);
 
     let x = -5;
     let y = x as f32;
-    assert!(y == -5.0);
-
-    let x = 9007199254740993i64;
-    assert!(x == 9007199254740993);
-    assert!(x as f32 == 9007199254740992.0);
+ 
+    let x = 9007199254740993i64 as f32;
 }

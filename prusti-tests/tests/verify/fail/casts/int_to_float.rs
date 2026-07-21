964b70953ea064de03250035a7b90e989074eea2
Thomas Mayerl <tmayerl@student.ethz.ch>
2026-07-21 17:32:31 +0200

    implement PR feedback

--- prusti-tests/tests/verify/fail/casts/int_to_float.rs
+++ prusti-tests/tests/verify/fail/casts/int_to_float.rs
@@ -4,21 +4,20 @@
 fn main() {
     let x = 100;
     let y = x as f32;
-    assert!(y == 100.0);
+    assert!(y == 100.0); //~ERROR: the asserted expression might not hold
 
     let x = u128::MAX;
     let y = x as f32;
-    assert!(y == f32::INFINITY);
+    assert!(y == f32::INFINITY); //~ERROR: the asserted expression might not hold
 
     let x = u8::MAX;
     let y = x as f64;
-    assert!(y == 255.0);
+    assert!(y == 255.0); //~ERROR: the asserted expression might not hold
 
     let x = -5;
     let y = x as f32;
-    assert!(y == -5.0);
+    assert!(y == -5.0); //~ERROR: the asserted expression might not hold
 
     let x = 9007199254740993i64;
-    assert!(x == 9007199254740993);
-    assert!(x as f32 == 9007199254740992.0);
+    assert!(x as f32 == 9007199254740992.0); //~ERROR: the asserted expression might not hold
 }

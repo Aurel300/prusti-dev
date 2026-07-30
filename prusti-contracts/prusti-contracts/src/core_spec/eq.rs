e60ee609e693d0e5d0c35f1375810167301e6e9f
Jonáš Fiala <jonas.fiala@inf.ethz.ch>
2026-07-30 18:47:18 +0200

    Specify `PartialEq` between references
    Relate equality of references to equality of their referents, for each
    combination of `&`/`&mut` on either side, so that `==` and `!=` can be
    moved through a reference in a specification.

--- prusti-contracts/prusti-contracts/src/core_spec/eq.rs
+++ prusti-contracts/prusti-contracts/src/core_spec/eq.rs
@@ -1,6 +1,6 @@
 use crate::*;
 
-use core::cmp::PartialEq;
+use core::{cmp::PartialEq, marker::PointeeSized};
 
 #[extern_spec]
 trait PartialEq<Rhs> {
@@ -27,6 +27,7 @@ impl PartialEq for () {
 
 #[extern_spec]
 impl<T: PartialEq> PartialEq for Option<T> {
+    #[trusted]
     #[pure]
     #[ensures(result == match (self, other) {
         (Some(l), Some(r)) => *l == *r,
@@ -35,6 +36,31 @@ impl<T: PartialEq> PartialEq for Option<T> {
     })]
     fn eq(&self, other: &Option<T>) -> bool;
 }
+
+macro_rules! impl_partial_eq_ref {
+    ($lhs:ty, $rhs:ty) => {
+        #[extern_spec]
+        impl<A: PointeeSized, B: PointeeSized> PartialEq<$rhs> for $lhs
+        where
+            A: PartialEq<B>,
+        {
+            #[trusted]
+            #[pure]
+            #[ensures(result == PartialEq::eq(*self, *other))]
+            fn eq(&self, other: &$rhs) -> bool;
+
+            #[trusted]
+            #[pure]
+            #[ensures(result == PartialEq::ne(*self, *other))]
+            fn ne(&self, other: &$rhs) -> bool;
+        }
+    };
+}
+
+impl_partial_eq_ref!(&A, &B);
+impl_partial_eq_ref!(&mut A, &mut B);
+impl_partial_eq_ref!(&A, &mut B);
+impl_partial_eq_ref!(&mut A, &B);
 
 /// Specifies that `PartialEq::eq`, if implemented, is a pure method, allowing its usage in specs.
 pub auto trait PureEq {}

7b65511c30f402a01a3e76497e6ecc6665366d3b
Aurea Bílá <aurel.bily@gmail.com>
2026-08-20 20:05:26 +0200

    report error for transmuting ptr-to-ptr casts

--- prusti-tests/tests/verify/pass/casts/ptr_to_ptr.rs
+++ prusti-tests/tests/verify/pass/casts/ptr_to_ptr.rs
@@ -4,6 +4,8 @@ fn main() {
     let mut x = 5;
     let p_x = &raw const x;
     let p_x = p_x as *mut i32;
-    unsafe { *p_x = 6 };
+    unsafe {
+        *p_x = 6;
+    }
     prusti_assert!(x == 6);
 }
